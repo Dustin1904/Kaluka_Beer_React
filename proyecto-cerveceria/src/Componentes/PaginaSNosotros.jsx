2502e8360a34c471f116d0645d26e0e6879cd40e
Dustin <dustin04ms@gmail.com>
2024-02-28 16:53:05 -0500

    otros cambios finales

--- proyecto-cerveceria/src/Componentes/PaginaSNosotros.jsx
+++ proyecto-cerveceria/src/Componentes/PaginaSNosotros.jsx
@@ -2,10 +2,45 @@ import React from "react";
 
 function PSNosotros(){
     return(
-      <div className="contenedor_sobre_nosotros">
-        <h1> KALUKA <span>BEER</span></h1>
-        
-      </div>
+      <div class="Pagina_Sobre_Nosotros">
+            
+            <div class="Contenido_Pagina">
+                <div class="tituloempresa">
+                    <h1 class="titulokaluka">KALUKA<span class="titulobeer">BEER</span></h1>
+                </div>
+                <div class="contenedor_tiempo">
+                    <div class="contenedor_historia">
+                        <div class="imagenFundadores"> 
+                            <img src="Imagenes/Fundadores3.jpeg" alt="Foto de los fundadores" />
+                            
+                        </div>
+                        <div class="contenedor_textoEmpresa">
+                            <h2 class="anioHistoria">2021</h2>
+                            <p class="textoEmpresa"> Nacidos desde un hobby y disfrute familiar, 
+                                comenzamos elaborando 5 litros de cerveza para compartir en familia bajo el nombre de NuNa Beer.</p>
+                        </div>
+                    </div>
+                    <div class="contenedor_historia">
+                        <div class="imagenFundadores"> 
+                            <img src="Imagenes/Fundadores2.jpeg" alt="Foto de los fundadores" />
+                        </div>
+                        <div class="contenedor_textoEmpresa">
+                            <h2 class="anioHistoria">2022</h2>
+                            <p class="textoEmpresa"> Por cuestines de registro de marca, nos llevo a renacer como Kaluka Beer, una marca que ha ido dejando una huella unica en la industria cervecera.</p>
+                        </div>
+                    </div>  
+                    <div class="contenedor_historia">
+                        <div class="imagenFundadores"> 
+                            <img src="Imagenes/Fundadores.jpeg" alt="Foto de los fundadores" />
+                        </div>
+                        <div class="contenedor_textoEmpresa">
+                            <h2 class="anioHistoria">2023</h2>
+                            <p class="textoEmpresa"> Detrás de Kaluka Beer estamos dos mentes apasionadas. Jessenia Basantes, quimica farmacéutica y líder en control de calidad, Cristhian Suárez, encargado de la publicidad y distribución.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 export default PSNosotros;
