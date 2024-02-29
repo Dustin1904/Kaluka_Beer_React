f18134ddcc83b0e619e9ffedb11b6f6606f35581
Alejandro <alejandro27campoverde@gmail.com>
2024-02-28 22:12:38 -0500

    SN

--- proyecto-cerveceria/src/Componentes/PaginaSNosotros.jsx
+++ proyecto-cerveceria/src/Componentes/PaginaSNosotros.jsx
@@ -1,41 +1,45 @@
 import React from "react";
+import '../Imagenes/Fundadores.jpeg'
+import '../Imagenes/Fundadores2.jpeg'
+import '../Imagenes/Fundadores3.jpeg'
+
+import '../css/sobre_nosotros_styles.css'
 
 function PSNosotros(){
     return(
-      <div class="Pagina_Sobre_Nosotros">
-            
-            <div class="Contenido_Pagina">
-                <div class="tituloempresa">
-                    <h1 class="titulokaluka">KALUKA<span class="titulobeer">BEER</span></h1>
+      <div className="Pagina_Sobre_Nosotros">
+            <div className="Contenido_Pagina">
+                <div className="tituloempresa">
+                    <h1 className="titulokaluka">KALUKA<span class="titulobeer">BEER</span></h1>
                 </div>
-                <div class="contenedor_tiempo">
-                    <div class="contenedor_historia">
-                        <div class="imagenFundadores"> 
-                            <img src="Imagenes/Fundadores3.jpeg" alt="Foto de los fundadores" />
+                <div className="contenedor_tiempo">
+                    <div className="contenedor_historia">
+                        <div className="imagenFundadores"> 
+                            <img src={require('../Imagenes/Fundadores.jpeg')} alt="Foto de los fundadores" />
                             
                         </div>
-                        <div class="contenedor_textoEmpresa">
-                            <h2 class="anioHistoria">2021</h2>
-                            <p class="textoEmpresa"> Nacidos desde un hobby y disfrute familiar, 
+                        <div className="contenedor_textoEmpresa">
+                            <h2 className="anioHistoria">2021</h2>
+                            <p className="textoEmpresa"> Nacidos desde un hobby y disfrute familiar, 
                                 comenzamos elaborando 5 litros de cerveza para compartir en familia bajo el nombre de NuNa Beer.</p>
                         </div>
                     </div>
-                    <div class="contenedor_historia">
-                        <div class="imagenFundadores"> 
-                            <img src="Imagenes/Fundadores2.jpeg" alt="Foto de los fundadores" />
+                    <div className="contenedor_historia">
+                        <div className="imagenFundadores"> 
+                            <img src={require('../Imagenes/Fundadores2.jpeg')} alt="Foto de los fundadores" />
                         </div>
-                        <div class="contenedor_textoEmpresa">
-                            <h2 class="anioHistoria">2022</h2>
-                            <p class="textoEmpresa"> Por cuestines de registro de marca, nos llevo a renacer como Kaluka Beer, una marca que ha ido dejando una huella unica en la industria cervecera.</p>
+                        <div className="contenedor_textoEmpresa">
+                            <h2 className="anioHistoria">2022</h2>
+                            <p className="textoEmpresa"> Por cuestines de registro de marca, nos llevo a renacer como Kaluka Beer, una marca que ha ido dejando una huella unica en la industria cervecera.</p>
                         </div>
                     </div>  
-                    <div class="contenedor_historia">
-                        <div class="imagenFundadores"> 
-                            <img src="Imagenes/Fundadores.jpeg" alt="Foto de los fundadores" />
+                    <div className="contenedor_historia">
+                        <div className="imagenFundadores"> 
+                            <img src={require('../Imagenes/Fundadores3.jpeg')} alt="Foto de los fundadores" />
                         </div>
-                        <div class="contenedor_textoEmpresa">
-                            <h2 class="anioHistoria">2023</h2>
-                            <p class="textoEmpresa"> Detrás de Kaluka Beer estamos dos mentes apasionadas. Jessenia Basantes, quimica farmacéutica y líder en control de calidad, Cristhian Suárez, encargado de la publicidad y distribución.</p>
+                        <div className="contenedor_textoEmpresa">
+                            <h2 className="anioHistoria">2023</h2>
+                            <p className="textoEmpresa"> Detrás de Kaluka Beer estamos dos mentes apasionadas. Jessenia Basantes, quimica farmacéutica y líder en control de calidad, Cristhian Suárez, encargado de la publicidad y distribución.</p>
                         </div>
                     </div>
                 </div>
